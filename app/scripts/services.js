91e6c7f26121aa8307b32b00224f848bf7ee5438
Jon Perl <perl.jonathan@gmail.com>
2013-03-25 18:34:28 -0400

    cucumber tests running now, can't do multiple features at once though yet

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -3,51 +3,51 @@
 angular.module("app")
 
     //local storage version
-//    .factory("todoStorage", function () {
-//        var STORAGE_ID = "todos-angularjs";
-//
-//        return {
-//            get: function (callback) {
-//                callback(JSON.parse(localStorage.getItem(STORAGE_ID) || "[]"));
-//            },
-//
-//            put: function (todos) {
-//                localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-//            }
-//        };
-//    });
-
-    //remote storage version
-    .factory("todoStorage", ["$http", "$waitDialog", function ($http, $waitDialog) {
-        var readUrl = "https://secure.openkeyval.org/";
-        var writeUrl = "https://secure.openkeyval.org/store/?";
-
-        var key = "JQueryMobileAngularTodoapp";
-
-        function get(callback) {
-            $waitDialog.show();
-            return $http({
-                method: "JSONP",
-                url: readUrl + key + "?callback=JSON_CALLBACK"
-            }).then(function (response) {
-                    $waitDialog.hide();
-                    callback(response.data);
-                });
-        }
-
-        function put(value) {
-            $waitDialog.show();
-            value = encodeURIComponent(JSON.stringify(value));
-            $http({
-                method: "JSONP",
-                url: writeUrl + key + "=" + value + "&callback=JSON_CALLBACK"
-            }).then(function () {
-                    $waitDialog.hide();
-                });
-        }
+    .factory("todoStorage", function () {
+        var STORAGE_ID = "todos-angularjs";
 
         return {
-            get: get,
-            put: put
+            get: function (callback) {
+                callback(JSON.parse(localStorage.getItem(STORAGE_ID) || "[]"));
+            },
+
+            put: function (todos) {
+                localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+            }
         };
-    }]);
+    });
+
+//    //remote storage version
+//    .factory("todoStorage", ["$http", "$waitDialog", function ($http, $waitDialog) {
+//        var readUrl = "https://secure.openkeyval.org/";
+//        var writeUrl = "https://secure.openkeyval.org/store/?";
+//
+//        var key = "JQueryMobileAngularTodoapp";
+//
+//        function get(callback) {
+//            $waitDialog.show();
+//            return $http({
+//                method: "JSONP",
+//                url: readUrl + key + "?callback=JSON_CALLBACK"
+//            }).then(function (response) {
+//                    $waitDialog.hide();
+//                    callback(response.data);
+//                });
+//        }
+//
+//        function put(value) {
+//            $waitDialog.show();
+//            value = encodeURIComponent(JSON.stringify(value));
+//            $http({
+//                method: "JSONP",
+//                url: writeUrl + key + "=" + value + "&callback=JSON_CALLBACK"
+//            }).then(function () {
+//                    $waitDialog.hide();
+//                });
+//        }
+//
+//        return {
+//            get: get,
+//            put: put
+//        };
+//    }]);
--- test/features/step_definitions/myStepDefinitions.js
+++ test/features/step_definitions/myStepDefinitions.js
@@ -1,38 +1,71 @@
 "use strict";
 
 addStepDefinitions(function (step) {
+    var uit, findTodo = function ($, text) {
+        return $("[todo]").find("span:contains('" + text + "')");
+    };
+
     step.Given("user opens the list", function (callback) {
-        callback();
-//        callback.pending();
-        //this.visit("http://localhost:9000/app/index.html", callback);
+        //create a new test every time
+        uit = uitest.create();
+        uit.feature("angularIntegration");
+
+        uit.url("../../app/index.html");
+
+        uit.ready(function () {
+            callback();
+        });
     });
 
-    step.When("user adds $todo to the list", function (todo, callback) {
-        callback();
-
-//        callback.pending();
+    step.When("user adds $todoText to the list", function (todoText, callback) {
+        uit.ready(function (document, angular) {
+            var input = angular.element("#todoInput");
+            input.val(todoText);
+            input.change();
+            angular.element("#addTodo").click();
+            callback();
+        });
     });
 
-    step.When("user completes $todo", function (todo, callback) {
-        callback();
-
-//        callback.pending();
+    step.When("user completes $todoText", function (todoText, callback) {
+        uit.ready(function (document, $, angular) {
+            var todoElement = findTodo($, todoText);
+            todoElement = angular.element(todoElement);
+            var scope = todoElement.scope();
+            scope.todo.completed = true;
+            scope.$digest();
+            callback();
+        });
     });
 
     step.When("user saves the list", function (callback) {
-        callback();
-
-//        callback.pending();
+        uit.ready(function (document, angular) {
+            angular.element("#save").click();
+            callback();
+        });
     });
 
-    step.Then("user should see $todo in the list", function (todo, callback) {
-        callback();
-
-//        callback.pending();
+    step.Then("user should see $todoText in the list", function (todoText, callback) {
+        uit.ready(function (document, $) {
+            var todo = findTodo($, todoText);
+            if (todo.length === 0) {
+                callback.fail();
+            }
+            else {
+                callback();
+            }
+        });
     });
 
-    step.Then("user should not see $todo in the list", function (todo, callback) {
-        callback();
-//        callback.pending();
+    step.Then("user should not see $todoText in the list", function (todoText, callback) {
+        uit.ready(function (document, $) {
+            var todo = findTodo($, todoText);
+            if (todo.length === 0) {
+                callback();
+            }
+            else {
+                callback.fail();
+            }
+        });
     });
 });
