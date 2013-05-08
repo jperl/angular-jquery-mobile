81c8e08e3c61d9edb1b6047457be1c7eca1339bb
Jon Perl <perl.jonathan@gmail.com>
2013-05-08 18:35:35 -0400

    setup better navigation / routes. simplified code a bit.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,27 @@
 "use strict";
 
 angular.module("app", [])
+    .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+        //bring back #!/ urls by turning OFF html5 paths
+        //read more here https://github.com/tigbro/jquery-mobile-angular-adapter/issues/163#issuecomment-17163635
+        $locationProvider.html5Mode(false);
+        $locationProvider.hashPrefix("!");
+
+        $routeProvider
+            //ex. http://localhost:9000/app/index.html#/about
+            .when("/", { templateUrl: "pages/main.html" })
+            .when("/about", { templateUrl: "pages/about.html" });
+
+        //prevent reloading the same page
+        $(document).bind("pagebeforechange", function (e, data) {
+            var to = data.toPage,
+                from = data.options.fromPage;
+
+            if (to && to.attr && from && from.attr && to.attr("id") === from.attr("id")) {
+                e.preventDefault();
+            }
+        });
+    }])
     .run(function () {
         //fixes the 1px jump on page load
         //https://github.com/jquery/jquery-mobile/issues/2846
